3754a5b08f4c3f5f6552ff8c08c4032aed757f37
Philipp Christoph <p.christoph@enbw.com>
2022-03-09 11:43:48 +0100

    Add nighttime switch.

---
+++ Apps/NighttimeSwitch/NighttimeSwitchConfig.cs
@@ -0,0 +1,11 @@
+﻿using HomeAutomations.Models;
+using HomeAutomations.Models.Generated;
+
+namespace HomeAutomations.Apps.NighttimeSwitch;
+
+public record NighttimeSwitchConfig : Config
+{
+	public string ScheduleCrontab { get; init; }
+	public SensorEntity LocationSensor { get; init; }
+	public InputNumberEntity InputNumber { get; init; }
+}
