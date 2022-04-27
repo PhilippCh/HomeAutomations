0beddcb4ccd3bb1d778408d48dac9d8de7241f93
Philipp Christoph <p.christoph@enbw.com>
2022-04-27 12:26:14 +0200

    Migrate instagram token update from node-red.

---
+++ HomeAutomations/Apps/UpdateInstagramToken/UpdateInstagramToken.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using HomeAutomations.Extensions;
+using HomeAutomations.Models;
+using HomeAutomations.Services;
+using Renci.SshNet;
+
+namespace HomeAutomations.Apps.UpdateInstagramToken;
+
+public class UpdateInstagramToken : BaseAutomation<UpdateInstagramToken, UpdateInstagramTokenConfig>
+{
+	private readonly NotificationService _notificationService;
+
+	public UpdateInstagramToken(BaseAutomationDependencyAggregate<UpdateInstagramToken, UpdateInstagramTokenConfig> aggregate, NotificationService notificationService)
+		: base(aggregate)
+	{
+		_notificationService = notificationService;
+	}
+
+	protected override Task StartAsync(CancellationToken cancellationToken)
+	{
+		StartUpdateCron(cancellationToken);
+
+		return Task.CompletedTask;
+	}
+
+	private async void StartUpdateCron(CancellationToken cancellationToken)
+	{
+		await CronjobExtensions.ScheduleJob(Config.UpdateCrontab, UpdateToken, true, cancellationToken);
+	}
+
+	private async void UpdateToken()
+	{
+		var client = new HttpClient();
+		var response = await client.GetAsync(Config.Url);
+
+		var connectionInfo = new ConnectionInfo(Config.Target.Host, Config.Target.Username, new PasswordAuthenticationMethod(Config.Target.Username, Config.Target.Password));
+
+		try
+		{
+			using var sftpClient = new SftpClient(connectionInfo);
+
+			sftpClient.Connect();
+			sftpClient.UploadFile(await response.Content.ReadAsStreamAsync(), Config.FilePath);
+		}
+		catch (Exception ex)
+		{
+			_notificationService.SendNotification(new Notification(Config.FailureNotification, new object[] { ex.Message }));
+		}
+	}
+}
---
+++ HomeAutomations/Apps/UpdateInstagramToken/UpdateInstagramTokenConfig.cs
@@ -0,0 +1,20 @@
+﻿using HomeAutomations.Common.Models.Config;
+using HomeAutomations.Models;
+
+namespace HomeAutomations.Apps.UpdateInstagramToken;
+
+public record SshConnectionConfig
+{
+	public string Host { get; init; }
+	public string Username { get; init; }
+	public string Password { get; init; }
+}
+
+public record UpdateInstagramTokenConfig : Config
+{
+	public string Url { get; init; }
+	public string UpdateCrontab { get; init; }
+	public string FilePath { get; init; }
+	public SshConnectionConfig Target { get; init; }
+	public Notification FailureNotification { get; init; }
+}
