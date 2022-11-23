44d4deb1fbfccc74d6df5af9ac3d2680cd836bbf
philippch <philipp@philippchristoph.de>
2022-11-23 09:43:26 +0100

    Add living room button.

---
+++ HomeAutomations/Apps/LivingRoomButton/LivingRoomButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using HomeAutomations.Models;
+using HomeAutomations.Models.DeviceMessages;
+using HomeAutomations.Models.Generated;
+
+namespace HomeAutomations.Apps.LivingRoomButton;
+
+public class LivingRoomButton : BaseAutomation<LivingRoomButton, LivingRoomButtonConfig>
+{
+	public LivingRoomButton(BaseAutomationDependencyAggregate<LivingRoomButton, LivingRoomButtonConfig> aggregate)
+		: base(aggregate)
+	{
+	}
+
+	protected override Task StartAsync(CancellationToken cancellationToken)
+	{
+		Config.Button.StateChanges().Subscribe(s => OnButtonPressed(s.New?.State));
+
+		return Task.CompletedTask;
+	}
+
+	private void OnButtonPressed(string? state)
+	{
+		var action = WirelessSwitchActions.Map(state);
+
+		if (action == ButtonAction.Single)
+		{
+			Config.StandardLamp.Toggle();
+		}
+	}
+}
---
+++ HomeAutomations/Apps/LivingRoomButton/LivingRoomButtonConfig.cs
@@ -0,0 +1,10 @@
+﻿using HomeAutomations.Common.Models.Config;
+using HomeAutomations.Models.Generated;
+
+namespace HomeAutomations.Apps.LivingRoomButton;
+
+public record LivingRoomButtonConfig : Config
+{
+	public SensorEntity Button { get; init; }
+	public LightEntity StandardLamp { get; init; }
+}
