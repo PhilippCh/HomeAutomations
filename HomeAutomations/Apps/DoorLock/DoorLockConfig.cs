a1ce0fe2b020bbdb238ca137e4578f22fabcceb1
philippch <philipp@philippchristoph.de>
2023-03-29 13:55:22 +0200

    Remove bridge api config.

--- HomeAutomations/Apps/DoorLock/DoorLockConfig.cs
+++ HomeAutomations/Apps/DoorLock/DoorLockConfig.cs
@@ -6,16 +6,8 @@ using HomeAutomations.Models.Generated;
 
 namespace HomeAutomations.Apps.DoorLock;
 
-public record NukiBridgeApiConfig
-{
-	public string BaseUrl { get; init; }
-	public string CallbackUrl { get; init; }
-	public string Token { get; init; }
-};
-
 public record DoorLockConfig : Config
 {
-	public NukiBridgeApiConfig BridgeApi { get; init; }
 	public string Token { get; init; }
 	public LockEntity LockEntity { get; init; }
 	public LockEntity OpenerEntity { get; init; }
