9e8a679500322fc005bde4c306401f75356fdebe
philippch <philipp@philippchristoph.de>
2024-05-16 23:05:26 +0200

    Refactor.

--- HomeAutomations.Common/Triggers/CompoundTrigger.cs
+++ HomeAutomations.Common/Triggers/CompoundTrigger.cs
@@ -6,33 +6,10 @@ namespace HomeAutomations.Common.Triggers;
 
 public class CompoundTrigger
 {
-	public IObservable<bool> Start
-	{
-		init
-		{
-			_start = value;
-			_startObserver?.Dispose();
-			_startObserver = _start.Where(x => x).Subscribe(_ => _subject.OnNext(true));
-		}
-	}
+	public IObservable<bool> Start { get; init; } = null!;
+	public IObservable<bool> End { get; init; } = null!;
 
-	public IObservable<bool> End
-	{
-		init
-		{
-			_end = value;
-			_endObserver?.Dispose();
-			_endObserver = _end.Where(x => x).Subscribe(_ => _subject.OnNext(false));
-		}
-	}
-
-	private readonly IObservable<bool> _end = null!;
-	private readonly IObservable<bool> _start = null!;
-	private readonly IDisposable? _startObserver;
-	private readonly IDisposable? _endObserver;
-	private readonly Subject<bool> _subject = new();
-
-	public IObservable<bool> AsObservable() => _start
-		.SwitchMap(x => x ? _end.Select(y => !y).StartWith(true) : false.AsObservable())
+	public IObservable<bool> AsObservable() => Start
+		.SwitchMap(x => x ? End.Select(y => !y).StartWith(true) : false.AsObservable())
 		.DistinctUntilChanged();
 }
